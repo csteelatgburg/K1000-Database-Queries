8cfae1fd8f54fc70b21355b7550121a37f440966
csteelatgburg <chuck@steelfarms.org>
2018-03-06 13:18:31 -0500

    Updating from local repository

---
+++ computers - apple computers with expiring adobe license.sql
@@ -0,0 +1,10 @@
+SELECT MACHINE.NAME, 
+substring_index(substring_index(PI.STR_FIELD_VALUE, '<br/>------------------------------------------------------------------------------------------------------<br/><br/>', 2), '<br/>------------------------------------------------------------------------------------------------------<br/><br/>', -1) as Expiry
+
+FROM ORG1.MACHINE
+LEFT JOIN MACHINE_CUSTOM_INVENTORY PI on MACHINE.ID = PI.ID and SOFTWARE_ID = 109220
+WHERE MACHINE.CS_MANUFACTURER like 'Apple%' 
+and STR_FIELD_VALUE is not null
+HAVING Expiry not like "<br/>No expiring/expired%"
+ORDER BY MACHINE.NAME
+
