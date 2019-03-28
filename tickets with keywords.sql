85da178197401534efd84cd3dafe440fc314d972
csteelatgburg <chuck@steelfarms.org>
2019-03-28 08:47:06 -0400

    One add an update
    Added patching query and updated tickets with keywords.sql

--- tickets with keywords.sql
+++ tickets with keywords.sql
@@ -1,11 +1,14 @@
-SELECT T.ID, T.TITLE, T.RESOLUTION,
+-- Tickets created in the past month that contain given keywords in the title, resolution, summary or comments
+
+
+SELECT T.ID, T.TITLE, T.RESOLUTION, T.SUMMARY,
 GROUP_CONCAT(C.COMMENT) as COMMENTS,
 CREATED
 FROM ORG1.HD_TICKET T
 JOIN HD_TICKET_CHANGE C on C.ID = T.ID
-WHERE MONTH(T.CREATED) = "08" 
-and YEAR(T.CREATED) = "2011"
+WHERE T.CREATED > DATE_SUB(NOW(), INTERVAL 1 MONTH)
 GROUP BY T.ID
 HAVING COMMENTS REGEXP 'malware|virus|combofix|mbam|ccleaner'
 or T.RESOLUTION REGEXP 'malware|virus|combofix|mbam|ccleaner'
 or T.TITLE REGEXP 'malware|virus|combofix|mbam|ccleaner'
+or T.SUMMARY REGEXP 'malware|virus|combofix|mbam|ccleaner'
